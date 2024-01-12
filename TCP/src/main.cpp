64bd808bd1843636a4b4bd7f9dd6933896255c9b
LYD666666 <2278373989@qq.com>
2024-01-12 16:14:21 +0800

    control timer and ration task

--- TCP/src/main.cpp
+++ TCP/src/main.cpp
@@ -1,7 +1,14 @@
 #include <WiFi.h>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <stdlib.h>
+using namespace std;
 
-const char *ssid = "LAINA_5G"; //wifi名
+const char *ssid = "LAINA"; //wifi名
 const char *password = "b8AxM8lqq";//wifi密码
+const char *timer_task = "timing"; //定时指令检测
+const char *ration_task = "ration";//定量指令检测
 
 const IPAddress serverIP(192,168,31,140); //欲访问的服务端IP地址
 uint16_t serverPort = 8080;         //服务端口号
@@ -28,22 +35,37 @@ void setup()
 
 void loop()
 {
-    Serial.println("尝试访问服务器");
+    Serial.println("try to question");
     if (client.connect(serverIP, serverPort)) //尝试访问目标地址
     {
-        Serial.println("访问成功");
+        Serial.println("success");
         client.print("Hello world!");                    //向服务器发送数据
         while (client.connected() || client.available()) //如果已连接或有收到的未读取的数据
         {
             if (client.available()) //如果有数据可读取
             {
                 String line = client.readStringUntil('\n'); //读取数据到换行符
-                Serial.print("读取到数据：");
-                Serial.println(line);
-                client.write(line.c_str()); //将收到的数据回发
+                //client.write(line.c_str()); //将收到的数据回发
+                String task_timer = strstr(line.c_str(),timer_task);
+                String task_ration = strstr(line.c_str(),ration_task);
+
+                if(task_timer != NULL)
+                {
+                    int index = line.indexOf(":");//获取第一个":"的位置
+                    String after1 = line.substring(index+1);
+                    int timer_number = after1.toInt();
+                    client.print(timer_number);
+                }
+                if(task_ration != NULL)
+                {
+                    int index = line.indexOf(":");//获取第一个":"的位置
+                    String after1 = line.substring(index+1);
+                    int ration_number = after1.toInt();
+                    client.print(ration_number);
+                }
             }
         }
-        Serial.println("关闭当前连接");
+        Serial.println("close");
         client.stop(); //关闭客户端
     }
     else
