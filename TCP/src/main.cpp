fb318f69fac05140b7be17df1cd6a046029156ad
LYD666666 <2278373989@qq.com>
2024-01-13 18:14:25 +0800

    “wificlient”

--- TCP/src/main.cpp
+++ TCP/src/main.cpp
@@ -11,11 +11,33 @@ const char *ssid = "LAINA"; //wifi名
 const char *password = "b8AxM8lqq";//wifi密码
 const char *timer_task = "timing"; //定时指令检测
 const char *ration_task = "ration";//定量指令检测
+String weight_task = "weight:";//称重指令检测
+String weight; //赋值实时称重量
+String jsonweight; //拼接成json格式发送
+
 
 const IPAddress serverIP(192,168,31,140); //欲访问的服务端IP地址
 uint16_t serverPort = 8080;         //服务端口号
 
 WiFiClient client; //声明一个ESP32客户端对象，用于与服务器进行连接
+TaskHandle_t weightHandle;
+
+// void sendweight(void *pt){
+//     while (1)
+//     {
+//         /* code */
+//         int num = rand()%100;//发送称重数据
+//         weight = String(num);
+//         jsonweight = weight_task + weight;
+//          if (client.connect(serverIP, serverPort)) //尝试访问目标地址
+//         {
+//         client.print(jsonweight);
+//         Serial.println(jsonweight);
+//         }
+//         vTaskDelay(6000/portTICK_PERIOD_MS); 
+//     }
+// }
+
 
 void setup()
 {
@@ -33,6 +55,8 @@ void setup()
     Serial.println("Connected");
     Serial.print("IP Address:");
     Serial.println(WiFi.localIP());
+
+    // xTaskCreate(sendweight,"sendweight",2024,NULL,1,&weightHandle);
 }
 
 void loop()
@@ -41,7 +65,12 @@ void loop()
     if (client.connect(serverIP, serverPort)) //尝试访问目标地址
     {
         Serial.println("success");
-        client.print("Hello world!");                    //向服务器发送数据
+        //client.print("Hello world!");                    //向服务器发送数据
+        int num = rand()%100;//发送称重数据
+        weight = String(num);
+        jsonweight = weight_task + weight;
+        client.print(jsonweight);
+
         while (client.connected() || client.available()) //如果已连接或有收到的未读取的数据
         {
             if (client.available()) //如果有数据可读取
@@ -54,15 +83,15 @@ void loop()
                 if(task_timer != NULL)
                 {
                     int index = line.indexOf(":");//获取第一个":"的位置
-                    String after1 = line.substring(index+1);
-                    int timer_number = after1.toInt();
+                    String timer_number = line.substring(index+1);
+                    //int timer_number = after1.toInt();
                     client.print(timer_number);
                 }
                 if(task_ration != NULL)
                 {
                     int index = line.indexOf(":");//获取第一个":"的位置
-                    String after1 = line.substring(index+1);
-                    int ration_number = after1.toInt();
+                    String ration_number = line.substring(index+1);
+                    //int ration_number = after1.toInt();
                     client.print(ration_number);
                 }
             }
@@ -75,5 +104,5 @@ void loop()
         Serial.println("访问失败");
         client.stop(); //关闭客户端
     }
-    delay(5000);
+    vTaskDelay(3000/portTICK_PERIOD_MS);
 }
